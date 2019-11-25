465ae60877df8852962c8605ccca6b746a01bdaa
cinyearchan <cinyearchan@gmail.com>
2019-11-25 22:31:04 +0800

    finish

--- examples/server.js
+++ examples/server.js
@@ -16,6 +16,46 @@ router.get('/simple/get', function (req, res) {
   })
 })
 
+router.get('/base/get', function (req, res) {
+  res.json(req.query)
+})
+
+router.post('/base/post', function (req, res) {
+  res.json(req.body)
+})
+
+router.post('/base/buffer', function (req, res) {
+  let msg = []
+  req.on('data', chunk => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
+})
+
+router.get('/error/get', function (req, res) {
+  if (Math.random() > 0.5) {
+    res.json({
+      msg: `Hello world`
+    })
+  } else {
+    res.status(500)
+    res.end()
+  }
+})
+
+router.get('/error/timeout', function (req, res) {
+  setTimeout(() => {
+    res.json({
+      msg: `Hello world`
+    })
+  }, 3000)
+})
+
 app.use(router)
 
 app.use(webpackDevMiddleware(compiler, {
@@ -33,7 +73,7 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
